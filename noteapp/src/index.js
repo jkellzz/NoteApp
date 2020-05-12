321b41bcfed29b6f8de526f21ddf30027b311b51
Johnathan Kelley <jkellz592@gmail.com>
2020-05-12 13:39:09 -0400

    on snapshot function is working. notes being called from firebase

--- noteapp/src/index.js
+++ noteapp/src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase')
+require('firebase/firestore')
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBdP0mh5WYIHa8fXDFWqdYWaq5j2y4wD0A",
+    authDomain: "noteapp-e0e5c.firebaseapp.com",
+    databaseURL: "https://noteapp-e0e5c.firebaseio.com",
+    projectId: "noteapp-e0e5c",
+    storageBucket: "noteapp-e0e5c.appspot.com",
+    messagingSenderId: "165172776892",
+    appId: "1:165172776892:web:64db418efcb7addeb625b9",
+    measurementId: "G-5WX5ET5DLD"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
